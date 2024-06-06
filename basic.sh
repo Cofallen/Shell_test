227fb20cad50e7c7bdafee8336ecfdc70804eb7b
root <root@.localdomain>
2024-06-06 23:56:02 +0800

    first

---
+++ basic.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# 打印绿色时间戳
+GREEN='\033[0;32m'  # 设置绿色颜色代码
+NC='\033[0m'       # 恢复默认颜色
+TIMESTAMP="$(date)"
+
+print_msg(){
+    MSG=$1
+    echo -e "${GREEN}[${TIMESTAMP}] ${MSG}${NC}"
+}
+
+# 检查是否有日志文件
+FILE=/home/cofallen/Bash-code/log.txt
+if [[ -f "${FILE}" ]];then
+    print_msg "清除残留日志文件..."
+    rm ${FILE}
+    print_msg "日志清理完成!!!"
+else 
+    print_msg "建立日志文件"
+    touch ${FILE}
+fi
+
+if [[ "$(id -u)" != "0" ]];then  # 获取UID
+    print_msg "This script must be run as root. Please run with sudo." | tee -a /home/cofallen/Bash-code/log.txt
+    exit 1
+fi
+
+print_msg "Shell script execution completed." | tee -a /home/cofallen/Bash-code/log.txt
